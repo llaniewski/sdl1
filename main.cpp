35342aef1b9c772995b47e75a7ddd50ccda21473
Lukasz Laniewski-Wollk <lukasz.laniewski@pw.edu.pl>
2025-08-04 11:22:59 +0200

    Simplified main

--- main.cpp
+++ main.cpp
@@ -9,13 +9,9 @@
 int main() {
 	graphics(400,400);
 	slow();
-	//clear();
 	line(50,50,350,350);
 	line(50,350,350,50);
-	circle(200,200,50);
-	circle(200,200,100);
 	circle(200,200,150);
-	text(200,200,"Some text");
 	wait();
 	return 0;
 }
